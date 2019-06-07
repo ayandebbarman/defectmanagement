5999d3102696790b44f70a6a4d38ae32da8a3a82
Deb Barman <ayan.deb.barman@accenture.com>
2019-06-07 14:43:08 +0530

    commenting logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import SimpleTable from './SimpleTable';
 import EnhancedTable from './enhancedTable'
